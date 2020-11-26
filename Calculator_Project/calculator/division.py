2aae9632f2dfa0cee5060347d07703aa6fbcbc5b
Your Name <you@example.com>
2020-11-26 19:42:06 +0530

    Updated the division.py File

--- Calculator_Project/calculator/division.py
+++ Calculator_Project/calculator/division.py
@@ -9,7 +9,7 @@ def divide():
     """
     This function divides the First Number by Second Number
     :return: Returns the resulting value after Division operation
-    i.e. The Result Value is Rounded upto 5 Decimal Places
+    i.e. The Result Value is Rounded up to 5 Decimal Places
     """
     first_num = float(input("Enter the First Value : "))
     second_num = float(input("Enter the Second Value : "))
@@ -36,3 +36,17 @@ def divide_2_numbers(first_num: float, second_num: float) -> float:
     print(f"Result of Division : {first_num}/{second_num} = {result_div:0.3f} ")
     return round(result_div, 3)
 
+
+# LAMBDA Function: To MULTIPLY given 2 Numbers
+# func_divide = lambda first_no, second_no: first_no / second_no if second_no != 0 else None
+# NOTE: Passing Lambda Function as 3rd Argument to below function..
+
+def divide_2_numbers_using_lambda(first_num: float, second_num: float, fn_divide: object) -> float:
+    """
+    This is to divide 2 numbers by using the Lambda Function
+    :param first_num:
+    :param second_num:
+    :param fn_divide:
+    :return: Returns the result of division by passing numbers to lambda function
+    """
+    return fn_divide(first_num, second_num)
